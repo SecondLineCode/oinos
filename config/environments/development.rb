ef5cc860b7a5b0cbb4c8fb2a6a9b4b8cb5d48995
Sia <siakaramalegos@gmail.com>
2015-03-20 14:33:16 -0500

    Re-seeded db to grab real admin account info and adjusted confirm options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
+  # TODO:  Fix email SMTP issues and turn this back on.
+  # TODO:  Format confirmation emails.
+  config.action_mailer.perform_deliveries = false
 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
