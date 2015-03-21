c458747d353778e1d36206950a13fe407caf5d44
Sia <siakaramalegos@gmail.com>
2015-03-20 19:17:22 -0500

    Search creates array hash with only info needed and filters out non-wine products.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   if params[:search]
     # Construct the URL we'll be calling
     request_uri = 'http://services.wine.com/api/beta2/service.svc/json/catalog?apikey=c880d81f0c6e496f2605bead17ddd36c'
-    request_type = '&size=10&search='
+    request_type = '&size=10&type="wine"&search='
     request_query = "#{params[:search]}"
     url = "#{request_uri}#{request_type}'#{request_query}'"
 
@@ -18,12 +18,34 @@ class PagesController < ApplicationController
 
     # Convert the String response into a plain old Ruby array. It is faster and saves you time compared to the standard Ruby libraries too.
     @result = JSON.parse(buffer)
-    @products = @result["Products"]["List"]
+    @raw_products = @result["Products"]["List"]
+    @products = Array.new
+
+    @raw_products.each do |wine|
+      # Filter out other products
+      if wine["Type"] == "Wine"
+        new_wine = Hash.new
+        new_wine["label"] = wine["Labels"][0]["Url"]
+        new_wine["name"] = wine["Name"]
+        new_wine["vineyard"] = wine["Vineyard"]["Name"]
+        new_wine["url"] = wine["Url"]
+        new_wine["appellation_region"] = wine["Appellation"]["Region"]["Name"]
+        new_wine["appellation_name"] = wine["Appellation"]["Name"]
+        new_wine["varietal_type"] = wine["Varietal"]["WineType"]["Name"]
+        new_wine["varietal_name"] = wine["Varietal"]["Name"]
+        new_wine["pricemin"] = wine["PriceMin"]
+        new_wine["pricemax"] = wine["PriceMax"]
+        new_wine["attributes"] = Array.new
+        wine["ProductAttributes"].each do |attribute|
+          new_wine["attributes"] << attribute["Name"]
+        end
+        @products << new_wine
+      end
+    end
 
     #TODO:  figure out how to grab the thumbnail or larger image, not the first image.
-    #TODO:  handle bad searches that either return nothing or return empty fields on the ones shown.
   else
-    @products = Array.new
+    @products
   end
 
 
